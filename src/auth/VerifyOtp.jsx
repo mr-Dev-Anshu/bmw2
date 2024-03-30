d35b83a7bc2e6e21a819c224620bf8c3610c5a5c
Prince Kumar Chaurasia <princechaurasiaofficial24@gmail.com>
2024-03-30 13:49:00 +0530

    xyz

--- src/auth/VerifyOtp.jsx
+++ src/auth/VerifyOtp.jsx
@@ -1,46 +1,55 @@
-import React, { useContext , useEffect, useState  } from 'react'
-import { AuthContext } from '../context/AuthContextProvider';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContextProvider";
+import { useNavigate } from "react-router-dom";
 
 export default function VerifyOtp() {
-     const [message , setMessage ] = useState () ; 
-     const [otp, setOtp] = useState();
-     const navigate = useNavigate() ; 
-     const {   VerifyOPT  , user } = useContext(AuthContext);
-      
-     useEffect(()=> {
-           if(user){
-             navigate("/") ; 
-           }
-     })
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState();
+  const [otp, setOtp] = useState();
+  const navigate = useNavigate();
+  const { VerifyOPT, user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/profile");
+    }
+  });
+
+  const handleVerification = () => {
+    try {
+      setMessage(null);
+      setLoading(true);
+      VerifyOPT(otp);
+      setLoading(false);
+    } catch (error) {
+      setMessage("Wrong OTP !");
+    }
+  };
   return (
     <div>
-      <div>
-          <div className="sm:mt-32  flex justify-center items-center  gap-8">
-            <input
-              value={otp}
-              className="border-b-2 border-slate-700 px-4 w- w-[20%] py-2  focus:outline-none"
-              placeholder="Enter OTP"
-              onChange={(e) => setOtp(e.target.value)}
-              type="text"
-            />
-            <div className="flex justify-center ">
-              <div>
-                <div className="">
-                  <button
-                    className="py-2 px-4 bg-slate-800 rounded text-white mt-2"
-                    onClick={()=> VerifyOPT(otp)}
-                  >
-                    Verify Your OTP
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center sm:mt-12 text-red-600 font-medium">
-            {message}
-          </div>
+      <p className="text-center mt-12 text-3xl text-green-500">
+        We have sent you an OTP on your Phone Number !
+      </p>
+      <div className="sm:mt-32  flex justify-center items-center  gap-8">
+        <input
+          required
+          className="border-b-2 text-xl border-slate-700 px-4 w- w-[20%] py-2  focus:outline-none"
+          placeholder="Enter OTP..."
+          onChange={(e) => setOtp(e.target.value)}
+          type="text"
+        />
+        <div className="flex justify-center ">
+          <button
+            className="py-2 px-8 font-semibold  bg-slate-800 rounded text-white mt-2"
+            onClick={handleVerification}
+          >
+            {loading ? "Loading..." : "Verify"}
+          </button>
         </div>
+      </div>
+      <div className="flex justify-center sm:mt-12 text-red-600 font-medium">
+        {message}
+      </div>
     </div>
-  )
+  );
 }
